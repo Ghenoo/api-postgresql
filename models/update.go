2b9c67f0f232d5944cc207a77f3761ebe4991e14
Ghenoo <lucasgheno600@gmail.com>
2024-03-12 11:01:31 -0300

    feat: fixing the coding

--- models/update.go
+++ models/update.go
@@ -2,7 +2,7 @@ package models
 
 import "api-postgresql/db"
 
-func Update(id int, todo Todo) (int64, error) {
+func Update(id int64, todo Todo) (int64, error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
 		return 0, err
